ac0bc8307d39351da21e9238fb10af8b278b3e6c
KangminLim <87487729+KangminLim@users.noreply.github.com>
2023-11-15 16:46:11 +0900

    Delete Greedy directory

--- Greedy/1026.py
+++
@@ -1,9 +0,0 @@
-N = int(input())
-A = list(map(int, input().split()))
-A.sort()
-B = list(map(int, input().split()))
-B.sort(reverse=True)
-S = 0
-for i in range(len(A)):
-    S += A[i] * B[i]
-print(S)
--- Greedy/11399.py
+++
@@ -1,9 +0,0 @@
-N = int(input())
-lst = list(map(int, input().split()))
-lst.sort()
-result= 0
-new_res = 0
-for i in range(N):
-    result += lst[i]
-    new_res += result
-print(new_res)
