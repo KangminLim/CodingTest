f155abadb92d6bec3fa3f97cc8549442253216b8
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-01-21 23:18:04 +0900

    Add files via upload

---
+++ 이것이코딩테스트다/12_1.py
@@ -0,0 +1,15 @@
+# N = str(input())
+# k = len(N)//2
+# lst1,lst2 = [],[]
+# sum1,sum2 = 0,0
+# for i in range(len(N)):
+#     if i<k:
+#         lst1.append(int(N[i]))
+#     else:
+#         lst2.append(int(N[i]))
+#
+# if sum(lst1) ==sum(lst2):
+#     print('LUCKY')
+# else:
+#     print('READY')
+
