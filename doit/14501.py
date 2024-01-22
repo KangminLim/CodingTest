5e4e1009bfc19f568043241e2025eb0808a46af4
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-01-22 19:39:00 +0900

    Add files via upload

---
+++ doit/14501.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+D = [0] * (N+2) # 오늘부터 퇴사일까지 벌 수 있는 최대 수입
+T = [0] * (N+1) # 상담에 필요한 일 수
+P = [0] * (N+1) # 상담 완료 시 수입
+for i in range(1,N+1):
+    T[i], P[i] = map(int,input().split())
+
+for i in range(N, 0, -1):
+    if i+T[i] > N+1:
+        D[i] =D[i+1]
+    else:
+        D[i] = max(D[i+1], P[i] + D[i+T[i]])
+print(D[1])
---
+++ doit/2747_BottomUp.py
@@ -0,0 +1,12 @@
+#피보나치 수열 바텀-업
+import sys
+input = sys.stdin.readline
+N = int(input())
+D = [-1] * (N+1)
+D[0] = 0
+D[1] = 1
+
+for i in range(2, N+1):
+    D[i] = D[i-2] + D[i-1]
+
+print(D[N])
