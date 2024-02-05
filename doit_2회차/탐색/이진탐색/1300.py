e9926ef6c4671dbe6f872d8f0b06f9d6819ac86e
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-02-05 22:05:56 +0900

    Add files via upload

---
+++ doit_2회차/탐색/이진탐색/1300.py
@@ -0,0 +1,20 @@
+N = int(input())
+K = int(input())
+start =1
+end = K
+ans = 0
+
+while start <= end:
+    middle = int((start+end)/2)
+    cnt = 0
+    # 중앙값보다 작은 수 계산
+
+    for i in range(1, N+1):
+        cnt += min(int(middle/i),N)
+    if cnt < K:
+        start = middle +1
+    else:
+        ans = middle
+        end = middle - 1
+
+print(ans)
