69654697feaf66854b0b91b54de0971a016cc327
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-02-07 18:48:57 +0900

    Add files via upload

---
+++ doit_2회차/정수론/소수 구하기/1016.py
@@ -0,0 +1,20 @@
+# 제곱이 아닌수
+import math
+Min, Max = map(int,input().split())
+Check = [False] * (Max-Min+1)
+
+for i in range(2, int(math.sqrt(Max)+1)):
+    pow = i * i
+    start_index =int(Min/pow)
+    if Min % pow != 0:
+        # 나머지가 있는 경우 1을 더해 Min보다 큰 제곱수에서 시작하도록 설정
+        start_index += 1
+    for j in range(start_index, int(Max/pow)+1):
+        Check[int((j*pow) - Min)] = True
+
+count = 0
+
+for i in range(0, Max-Min+1):
+    if not Check[i]:
+        count += 1
+print(count)
