50f14546d6704c9ad64ca80bd89447f613bdf272
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-02-15 19:34:47 +0900

    Add files via upload

---
+++ doit_2회차/그래프/최소신장트리/1197.py
@@ -0,0 +1,39 @@
+import sys
+from queue import PriorityQueue
+
+input = sys.stdin.readline
+N, M = map(int,input().split())
+pq = PriorityQueue()
+parent = [0] * (N+1)
+
+for i in range(N+1):
+    parent[i] = i
+
+for i in range(M):
+    s,e,w = map(int,input().split())
+    pq.put((w,s,e))
+
+def find(a):
+    if a==parent[a]:
+        return a
+    else:
+        parent[a] = find(parent[a])
+        return parent[a]
+
+def union(a,b):
+    a = find(a)
+    b = find(b)
+    if a != b:
+        parent[b] = a
+
+useEdge = 0
+result = 0
+
+while useEdge < N-1:
+    w,s,e = pq.get()
+    if find(s) != find(e):
+        union(s,e)
+        result += w
+        useEdge += 1
+
+print(result)
