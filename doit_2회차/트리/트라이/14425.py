37ceddbf74a40eded2b794ee73524deba7a51a4e
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-02-19 18:46:47 +0900

    Add files via upload

---
+++ doit_2회차/트리/트라이/14425.py
@@ -0,0 +1,50 @@
+from sys import stdin
+input = stdin.readline
+
+class Node(object):
+    def __init__(self, isEnd):
+        self.isEnd = isEnd
+        self.childNode = {}
+
+class Trie(object):
+    def __init__(self):
+        self.parent = Node(None)
+
+    def insert(self, string):
+        nowNode = self.parent
+        temp_length = 0
+        for char in string:
+            if char not in nowNode.childNode:
+                nowNode.childNode[char] = Node(char)
+            nowNode = nowNode.childNode[char]
+            temp_length += 1
+            if temp_length == len(string):
+                nowNode.isEnd = True
+
+    def search(self, string):
+        nowNode = self.parent
+        temp_length = 0
+        for char in string:
+            if char in nowNode.childNode:
+                nowNode = nowNode.childNode[char]
+                temp_length += 1
+                if temp_length == len(string) and nowNode.isEnd == True:
+                    return True
+            else:
+                return False
+        return False
+
+N, M = map(int,input().split())
+myTrie = Trie()
+
+for _ in range(N):
+    word = input().strip()
+    myTrie.insert(word)
+
+result = 0
+for _ in range(M):
+    word = input().strip()
+    if myTrie.search(word):
+        result += 1
+
+print(result)
---
+++ doit_2회차/트리/트리기초/11725.py
@@ -0,0 +1,27 @@
+#트리의 부모찾기
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+N = int(input())
+visited = [False] *(N+1)
+tree =[[] for _ in range(N+1)]
+answer = [0] * (N+1)
+
+for _ in range(1,N):
+    n1, n2 = map(int,input().split())
+    tree[n1].append(n2)
+    tree[n2].append(n1)
+
+def DFS(number):
+    visited[number] = True
+    for i in tree[number]:
+        if not visited[i]:
+            answer[i] = number
+            DFS(i)
+
+DFS(1) # 부모 노드부터 DFS 시작
+
+for i in range(2,N+1):
+    print(answer[i])
+
