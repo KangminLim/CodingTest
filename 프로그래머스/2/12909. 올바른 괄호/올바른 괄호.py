29b3131065ade213a5e011729410a619975b4522
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-03-07 12:01:44 +0900

    [level 2] Title: 올바른 괄호, Time: 11.58 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,15 @@
+def solution(s):
+    
+    if s[0] != '(':
+            return False
+        
+    stack = []
+    for i in range(len(s)):
+        if s[i] == '(':
+            stack.append(s[i])
+        elif stack and s[i] == ')':
+            stack.pop()
+    if stack:
+        return False
+
+    return True
