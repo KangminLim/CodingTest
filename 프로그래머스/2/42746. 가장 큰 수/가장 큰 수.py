7c418ef5ccefee3c4abe6c2f9f8274cee2ddf124
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-03-08 12:48:19 +0900

    [level 2] Title: 가장 큰 수, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    str_numbers = list(map(str,numbers))
+    length = len(numbers)
+
+    str_numbers.sort(key = lambda x:x*3,reverse=True)
+    answer = str(int(''.join(str_numbers)))
+    
+    return answer
