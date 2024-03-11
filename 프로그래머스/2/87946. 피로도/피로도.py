7f2ee27e787ac34a3f228477a3b447b74f54a50f
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-03-11 09:56:19 +0900

    [level 2] Title: 피로도, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -0,0 +1,18 @@
+answer = 0
+# visited = []
+
+def dfs(k, cnt, dungeons):
+    global answer
+    if cnt >= answer:
+        answer = cnt
+    for i in range(len(dungeons)):
+        if k >= dungeons[i][0] and not visited[i]:
+            visited[i] = True
+            dfs(k-dungeons[i][1], cnt+1, dungeons)
+            visited[i] = False
+    
+def solution(k,dungeons):
+    global visited
+    visited = [False] * len(dungeons)
+    dfs(k,0,dungeons)
+    return answer
