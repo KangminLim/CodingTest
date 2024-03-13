74fc28bbe6b6c796d37218f67a9074385c4939f9
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-03-13 14:38:10 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -0,0 +1,14 @@
+def solution(n, costs):
+    # 크루스칼 
+    answer = 0
+    costs.sort(key=lambda x:x[2])
+    connect = set([costs[0][0]])
+    while len(connect) != n:
+        for cost in costs:
+            if cost[0] in connect and cost[1] in connect: # 사이클 형성을 막음
+                continue
+            if cost[0] in connect or cost[1] in connect: # 기존 간선과 이져야함
+                connect.update([cost[0], cost[1]])
+                answer += cost[2]
+                break
+    return answer
