aef05b2c669ccb7e6ee5c01d8f26c2b710563574
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-03-15 10:16:33 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,12 +1,13 @@
 def solution(clothes):
     clo = {}
     for clothe in clothes:
-        if clothe[1] not in clo.keys():
+        if clothe[1] not in clo:
             clo[clothe[1]] = 1
         else:
             clo[clothe[1]] += 1
-    ans = 1
-    for key, value in clo.items():
-        ans *= (value+1)
-        
-    return ans-1
+            
+    answer = 1
+    for idx, val in clo.items():
+        answer *= (val+1) 
+    
+    return answer-1
