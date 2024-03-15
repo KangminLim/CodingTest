de0f87b76509ecf60e9b77ef5b0bc5f7b02e95b2
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-03-15 10:47:23 +0900

    [level 3] Title: 베스트앨범, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,29 +1,27 @@
 def solution(genres, plays):
     answer = []
-    total_dict = {}
-    genre_dict = {}
-    
-    for i in range(len(genres)):
-        # 장르 합
-        if genres[i] not in total_dict:
-            total_dict[genres[i]] = plays[i]
+    N = len(genres)
+    g_dict = {}
+    t_dict = {}
+    for i in range(N):
+        if genres[i] not in g_dict:
+            g_dict[genres[i]] = plays[i]
         else:
-            total_dict[genres[i]] += plays[i]
-        # 장르 정보 
-        if genres[i] not in genre_dict:
-            genre_dict[genres[i]] = [[plays[i],i]]
+            g_dict[genres[i]] += plays[i]
+        
+        if genres[i] not in t_dict:
+            t_dict[genres[i]] = [[plays[i],i]]
         else:
-            genre_dict[genres[i]].append([plays[i],i])
+            t_dict[genres[i]].append([plays[i],i])
     
-    genre_rank = sorted(total_dict, key=total_dict.get, reverse=True)
+    genre_rank = sorted(g_dict, key=g_dict.get, reverse = True)
     
     for x in genre_rank:
-        play_rank = sorted(genre_dict[x], key=lambda x:(-x[0],x[1]))
+        total_rank = sorted(t_dict[x], key= lambda x : (-x[0],x[1]))
         
-        if len(play_rank) == 1:
-            answer.append(play_rank[0][1])
+        if len(total_rank) == 1:
+            answer.append(total_rank[0][1])
         else:
             for i in range(2):
-                answer.append(play_rank[i][1])
-    
+                answer.append(total_rank[i][1])
     return answer
