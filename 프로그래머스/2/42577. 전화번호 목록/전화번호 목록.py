3b57addcb835ad2a11ea3fad89617d05468c0ede
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-03-15 10:06:00 +0900

    [level 2] Title: 전화번호 목록, Time: 103.11 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,12 +1,4 @@
 def solution(phone_book):
-    
-    # for phone in phone_book:
-    #     for i in range(len(phone_book)):
-    #         if phone == phone_book[i]:
-    #             continue
-    #         if phone in phone_book[i]:
-    #             return False
-    # return True
     answer = True
     phone_book.sort()
     for i in range(len(phone_book)-1):
