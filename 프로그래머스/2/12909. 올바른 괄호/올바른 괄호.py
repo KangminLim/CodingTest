8ea28df8fcd8bdff4d8ce9402c97c22846e5b25b
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-03-18 15:33:22 +0900

    [level 2] Title: 올바른 괄호, Time: 25.97 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,14 +1,17 @@
 def solution(s):
+    answer = True
     if s[0] == ')':
         return False
-    
     stack = []
     for i in range(len(s)):
+        if i == 0: 
+            stack.append(s[i])
+            continue
         if s[i] == '(':
             stack.append(s[i])
-        elif stack and s[i] ==')':
+        elif stack and s[i] == ')':
             stack.pop()
+            
     if stack:
         return False
-    
     return True
