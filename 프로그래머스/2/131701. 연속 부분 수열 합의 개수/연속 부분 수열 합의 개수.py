f3256eeff2df5290dfc5574800ad02e39856da12
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-03-18 19:28:46 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 5038.84 ms, Memory: 43.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -0,0 +1,11 @@
+def solution(elements):
+    tset = set()
+    e_len =len(elements)
+    elements = elements*2
+    for i in range(e_len):
+        temp = elements[i]
+        tset.add(temp)
+        for j in range(e_len):
+            temp = sum(elements[j:j+1+i])
+            tset.add(temp)
+    return len(tset)
