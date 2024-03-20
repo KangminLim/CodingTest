6be2330942286387eff021cf4d4f67bfa0fde3ff
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-03-20 09:13:21 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -0,0 +1,10 @@
+def solution(skill, skill_trees):
+    answer = 0
+    for tree in skill_trees:
+        s = ''
+        for ch in tree:
+            if ch in skill:
+                s += ch
+        if skill[:len(s)] == s:
+            answer += 1
+    return answer
