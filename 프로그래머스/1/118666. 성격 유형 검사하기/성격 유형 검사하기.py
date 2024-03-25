f963d2bb9d90523e4bfb2ff881c492bdc5a24700
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-03-25 21:42:03 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.38 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -0,0 +1,35 @@
+def solution(survey, choices):
+    answer = ''
+    N = len(survey)
+    s_dict = {}
+    c_dict = {1:3,2:2,3:1,4:0,5:1,6:2,7:3}
+    sur = 'RTCFJMAN'
+    for i in sur:
+        s_dict[i] = 0
+        
+    for i in range(N):
+        sur = survey[i]
+        cho = choices[i]
+        if 1<=cho<=3:
+            s_dict[sur[0]] += c_dict[cho] 
+        elif 4<=cho<=7:
+            s_dict[sur[1]] += c_dict[cho]
+    
+    if s_dict['R'] >= s_dict['T']:
+        answer += 'R'
+    else : answer += 'T'
+    
+    if s_dict['C'] >= s_dict['F']:
+        answer += 'C'
+    else : answer += 'F'
+    
+    if s_dict['J'] >= s_dict['M']:
+        answer += 'J'
+    else : answer += 'M'
+    
+    if s_dict['A'] >= s_dict['N']:
+        answer += 'A'
+    else : answer += 'N'
+    
+    
+    return answer
