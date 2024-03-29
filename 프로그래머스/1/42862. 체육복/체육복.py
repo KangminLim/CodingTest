f358334662418e6ed8775f4def13592d4cc6219c
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-03-29 17:21:28 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,15 +1,14 @@
 def solution(n, lost, reserve):
-
-    f_reserve = [r for r in reserve if r not in lost]
-    f_lost = [l for l in lost if l not in reserve]
-    f_reserve.sort()
-    for r in f_reserve:
-        f = r-1
-        b = r+1
+    lost.sort()
+    reserve.sort()
+    # lost, reserve 겹친 애 뺴기
+    r_lost = set(lost) - set(reserve)
+    r_reserve = set(reserve) - set(lost)
+    for i in r_reserve:
+        if i-1 in r_lost:
+            r_lost.remove(i-1)
+        elif i+1 in r_lost:
+            r_lost.remove(i+1)
         
-        if f in f_lost:
-            f_lost.remove(f)
-        elif b in f_lost:
-            f_lost.remove(b)
+    return n-len(r_lost)
     
-    return n - len(f_lost)
