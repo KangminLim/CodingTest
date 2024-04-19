275f768ab0081fcb582f87df9b6fa5d075c646ee
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-04-19 15:16:16 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.98 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,34 +1,35 @@
 from collections import deque
-
 def solution(n, wires):
-    result = n
     graph = [[] for _ in range(n+1)]
-    for a,b in wires:
+    
+    for a, b in wires:
         graph[a].append(b)
         graph[b].append(a)
-    
+
     def bfs(x):
-        visited = [False] * (n+1)
         q = deque()
         q.append(x)
-        visited[x] = True
-        cnt = 1
-        while q:
-            s = q.popleft()
-            for i in graph[s]:
-                if not visited[i]:
-                    q.append(i)
-                    visited[i] = 1
+        v = [False] * (n+1)
+        v[x] = True
+        cnt= 1
+        while q: 
+            cur = q.popleft()
+            for node in graph[cur]:
+                if not v[node]:
+                    q.append(node)
+                    v[node] = True
                     cnt += 1
         return cnt
-        
-    for a,b in wires:
+    
+    result = n
+    
+    for a, b in wires:
         graph[a].remove(b)
         graph[b].remove(a)
         
-        result = min(abs(bfs(a)-bfs(b)),result)
+        result = min(abs(bfs(a)-bfs(b)), result)
         
         graph[a].append(b)
         graph[b].append(a)
-    
+
     return result
