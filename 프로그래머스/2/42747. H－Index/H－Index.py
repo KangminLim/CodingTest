42df5eb18c8a24e49a14a797a175beff8d58b108
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-04-22 21:53:40 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,9 +1,9 @@
 def solution(citations):
     citations.sort(reverse=True)
-    length= len(citations)
+    length = len(citations)
     for idx, citation in enumerate(citations):
         if idx >= citation:
             return idx
-    if max(citations)==0:
+    if max(citations) == 0:
         return 0 
     return length
