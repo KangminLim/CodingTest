c05a9a04e45fd52f3ae1aed93947dc94af70fc00
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-04-22 22:00:52 +0900

    [level 2] Title: 프로세스, Time: 0.21 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,17 +1,17 @@
 from collections import deque
 def solution(priorities, location):
-    answer = 0
     q = deque()
+    answer = 0
     for idx, val in enumerate(priorities):
         q.append((val,idx))
-    
+        
     while q:
         item = q.popleft()
-        if q and item[0] < max(q)[0] :
+        if q and item[0] < max(q)[0]:
             q.append(item)
         else:
             answer += 1
             if item[1] == location:
                 break
-                
+
     return answer
