9befe91cf7dfc49de52a87a332ea8df411d14bfb
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-04-24 14:42:43 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,7 +1,7 @@
 def solution(progresses, speeds):
     answer = []
     days, cnt = 0, 0
-    while len(progresses) >0:
+    while progresses:
         if progresses[0] + speeds[0] * days >= 100:
             progresses.pop(0)
             speeds.pop(0)
@@ -11,5 +11,6 @@ def solution(progresses, speeds):
                 answer.append(cnt)
                 cnt = 0
             days += 1
+            
     answer.append(cnt)
     return answer
