1fea3512b6557e42154cc67e7278550220c38c54
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-04-26 15:11:38 +0900

    [level 2] Title: 롤케이크 자르기, Time: 790.05 ms, Memory: 51.3 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,15 +1,23 @@
-from collections import Counter
 def solution(topping):
-    left = Counter(topping)
-    right = set()
+    tdict = {}
+    for t in topping:
+        if t not in tdict:
+            tdict[t] = 1
+        else:
+            tdict[t] += 1
+    
+    adict = {}    
     answer = 0
     for i in topping:
-        left[i] -= 1
-        right.add(i)
+        tdict[i] -= 1
+        if i not in adict:
+            adict[i] = 1
+        else: 
+            adict[i] += 1
+            
+        if tdict[i] == 0:
+            tdict.pop(i)
         
-        if left[i] == 0:
-            left.pop(i)
-        
-        if len(left) == len(right):
-            answer += 1
+        if len(tdict) == len(adict):
+            answer += 1 
     return answer
