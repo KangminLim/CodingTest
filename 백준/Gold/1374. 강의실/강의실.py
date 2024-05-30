d9f34a86b7014a294a0f02cddf449365bb3d0586
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-05-30 22:43:47 +0900

    [Gold V] Title: 강의실, Time: 4496 ms, Memory: 64212 KB -BaekjoonHub

---
+++ 백준/Gold/1374. 강의실/강의실.py
@@ -0,0 +1,16 @@
+import heapq
+
+N = int(input())
+lst = [list(map(int,input().split())) for _ in range(N)]
+lst.sort(key=lambda x:(x[1],x[2]))
+
+pq = []
+heapq.heappush(pq,lst[0][2])
+
+for i in range(1,N):
+    if lst[i][1] >= pq[0]:
+        heapq.heappop(pq)
+    heapq.heappush(pq,lst[i][2])
+
+print(len(pq))
+
