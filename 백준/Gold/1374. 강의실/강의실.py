bbac709e5a1c9c93a6e25311a7853b1cb1a7d2a7
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-05-31 13:56:22 +0900

    [Gold V] Title: 강의실, Time: 4280 ms, Memory: 64212 KB -BaekjoonHub

--- 백준/Gold/1374. 강의실/강의실.py
+++ 백준/Gold/1374. 강의실/강의실.py
@@ -1,16 +1,15 @@
+# 강의실
 import heapq
-
 N = int(input())
 lst = [list(map(int,input().split())) for _ in range(N)]
-lst.sort(key=lambda x:(x[1],x[2]))
 
+lst.sort(key=lambda x:(x[1],x[2]))
 pq = []
-heapq.heappush(pq,lst[0][2])
+heapq.heappush(pq,lst[0][2]) # 맨처음 종료 시간 넣기
 
 for i in range(1,N):
     if lst[i][1] >= pq[0]:
         heapq.heappop(pq)
     heapq.heappush(pq,lst[i][2])
 
-print(len(pq))
-
+print(len(pq)) # pq가 강의실 개수
