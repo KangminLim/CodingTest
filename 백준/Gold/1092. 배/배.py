975cf5f2ad0a2ffdc2e3e089c460639a1b352062
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-06-12 12:53:00 +0900

    [Gold V] Title: 배, Time: 56 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.py
+++ 백준/Gold/1092. 배/배.py
@@ -5,17 +5,11 @@ M = int(input())
 boxes = list(map(int,input().split()))
 boxes.sort(reverse=True)
 cnt = 0
-i = 0
-
-# 모든 박스를 배로 못 옮길 경우
 if cranes[0] < boxes[0]:
     print(-1)
 else:
-    # 박스를 다 옮기면 종료
     while boxes:
-        # 가장 무거운 크레인부터
         for crane in cranes:
-            # 박스를 다 옮기지 못하고 남은 크레인으로 옮길 박스가 없을 때
             if boxes and crane < boxes[-1]:
                 break
             for box in boxes:
@@ -23,5 +17,4 @@ else:
                     boxes.remove(box)
                     break
         cnt += 1
-
     print(cnt)
