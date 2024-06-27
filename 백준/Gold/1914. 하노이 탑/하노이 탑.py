df2f3fdd5803a432496b806a4d7de72dad454cea
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-06-27 23:11:56 +0900

    [Gold V] Title: 하노이 탑, Time: 856 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.py
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -1,13 +1,13 @@
-def hanoi(N,start,end):
+def hanoi(N,start,mid,end):
     if N == 1:
         print(start,end)
         return
 
-    hanoi(N-1,start,6-start-end) # 1단계 (1->2)
+    hanoi(N-1,start,end,mid) # 1단계 (1->2)
     print(start,end) # 2단계 (1의 마지막 원반 -> 3)
-    hanoi(N-1,6-start-end,end) # 3단계 (2->3)
+    hanoi(N-1,mid,start,end) # 3단계 (2->3)
 
 N = int(input())
 print(2**N-1)
 if N <= 20:
-    hanoi(N,1,3)
+    hanoi(N,1,2,3)
