8baca596a184d847fe19c5816e11a7a7f8ea7f80
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-07-12 10:11:32 +0900

    [level 2] Title: 괄호 변환, Time: 0.14 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
@@ -1,35 +1,50 @@
-def balanced_index(p):
-    count = 0 # 왼쪽 괄호의 개수
+'''
+'(' 와 ')' 로만 이루어진 문자열이 있을 경우, '(' 의 개수와 ')' 의 개수가 같다면 이를 균형잡힌 괄호 문자열이라고 부릅니다.
+그리고 여기에 '('와 ')'의 괄호의 짝도 모두 맞을 경우에는 이를 올바른 괄호 문자열이라고 부릅니다.
+1. 입력이 빈 문자열인 경우, 빈 문자열을 반환합니다. 
+2. 문자열 w를 두 "균형잡힌 괄호 문자열" u, v로 분리합니다. 단, u는 "균형잡힌 괄호 문자열"로 더 이상 분리할 수 없어야 하며, v는 빈 문자열이 될 수 있습니다. 
+3. 문자열 u가 "올바른 괄호 문자열" 이라면 문자열 v에 대해 1단계부터 다시 수행합니다. 
+  3-1. 수행한 결과 문자열을 u에 이어 붙인 후 반환합니다. 
+4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다. 
+  4-1. 빈 문자열에 첫 번째 문자로 '('를 붙입니다. 
+  4-2. 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다. 
+  4-3. ')'를 다시 붙입니다. 
+  4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다. 
+  4-5. 생성된 문자열을 반환합니다.
+'''
+# 균형잡힌 괄호 문자열 인덱스 반환
+def check_balance(p):
+    cnt = 0
     for i in range(len(p)):
-        if p[i] == '(': 
-            count += 1
+        if p[i] == '(':
+            cnt += 1
         else:
-            count -= 1
-        if count == 0:
+            cnt -= 1 
+        if cnt == 0:
             return i
-        
+
+# 올바른 문자열
 def check_proper(p):
-    count = 0 # 왼쪽 괄호의 개수
+    cnt = 0
     for i in p:
         if i == '(':
-            count += 1
+            cnt += 1
         else:
-            if count == 0: # 쌍이 맞지 않는 경우에 False 반환
-                return False
-            count -= 1
-    return True # 쌍이 맞는 경우에 True 반환 
+            if cnt == 0:
+                return False # 쌍이 맞지 않는 경우
+            cnt -= 1
+    return True
+    
 
 def solution(p):
     answer = ''
-    if p == '':
-        return answer
-    index = balanced_index(p)
-    u = p[:index + 1]
-    v = p[index + 1:]
-    # "올바른 괄호 문자열"이면, v에 대해 함수를 수행한 결과를 붙여 반환
+    if p == '': return ''
+    idx = check_balance(p)
+    u = p[:idx+1]
+    v = p[idx+1:]
+    
     if check_proper(u):
         answer = u + solution(v)
-    # 아니라면 아래의 과정
     else:
         answer = '('
         answer += solution(v)
@@ -41,7 +56,6 @@ def solution(p):
             else:
                 u[i] = '('
         answer += ''.join(u)
+                    
+
     return answer
-
-
-
