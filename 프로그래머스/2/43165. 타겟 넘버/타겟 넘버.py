e1a3702892745a6740662de789d277c581d6ee0c
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-07-15 09:53:02 +0900

    [level 2] Title: 타겟 넘버, Time: 415.83 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,12 +1,15 @@
+
 def solution(numbers, target):
     answer = 0
-    def dfs(n,sm):
-        nonlocal answer 
-        if n==len(numbers):
-            if sm ==target:    
-                answer +=1
+    def backtracking(sm, n):
+        nonlocal answer
+        if n == len(numbers):
+            if sm == target:
+                answer += 1
             return
-        dfs(n+1,sm+numbers[n])
-        dfs(n+1,sm-numbers[n])
-    dfs(0,0)
+        backtracking(sm + numbers[n],n+1)
+        backtracking(sm - numbers[n],n+1)
+
+    backtracking(0,0)
+
     return answer
