0c3329aba1bedc6f32975fd863dae48fdecc329b
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-08-14 09:44:54 +0900

    [level 3] Title: 부대복귀, Time: 838.58 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.py
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.py
@@ -1,22 +1,3 @@
-from collections import deque
 def solution(n, roads, sources, destination):
     answer = []
-    v = [-1] * (n+1)
-    graph =[[] for _ in range(n+1)]
-    
-    for a,b in roads:
-        graph[a].append(b)
-        graph[b].append(a)
-
-    q = deque()
-    q.append(destination)
-    v[destination] += 1
-    while q:
-        cur = q.popleft()
-        for i in graph[cur]:
-            if v[i] == -1:
-                q.append(i)
-                v[i] = v[cur] + 1
-    for i in sources:
-        answer.append(v[i])
     return answer
