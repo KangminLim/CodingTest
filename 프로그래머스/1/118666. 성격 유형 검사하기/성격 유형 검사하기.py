1b39a5c87d1a4478a4be3d27fe3c1ab84bc00454
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-08-28 09:48:23 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.52 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,35 +1,58 @@
 def solution(survey, choices):
-    answer = ''
-    N = len(survey)
-    s_dict = {}
-    c_dict = {1:3,2:2,3:1,4:0,5:1,6:2,7:3}
-    sur = 'RTCFJMAN'
-    for i in sur:
-        s_dict[i] = 0
+    R, T, F, C, M, J, A, N = 0,0,0,0,0,0,0,0
+    cdict = {1:3, 2:2, 3:1, 5:1, 6:2, 7:3}
+    for i in range(len(survey)):
+        cur_s = survey[i]
+        cur_c = choices[i]
         
-    for i in range(N):
-        sur = survey[i]
-        cho = choices[i]
-        if 1<=cho<=3:
-            s_dict[sur[0]] += c_dict[cho] 
-        elif 5<=cho<=7:
-            s_dict[sur[1]] += c_dict[cho]
-    
-    if s_dict['R'] >= s_dict['T']:
-        answer += 'R'
-    else : answer += 'T'
-    
-    if s_dict['C'] >= s_dict['F']:
-        answer += 'C'
-    else : answer += 'F'
-    
-    if s_dict['J'] >= s_dict['M']:
-        answer += 'J'
-    else : answer += 'M'
-    
-    if s_dict['A'] >= s_dict['N']:
-        answer += 'A'
-    else : answer += 'N'
-    
+        if cur_c >=5:
+            idx = 1
+            if cur_s[idx] == 'R':
+                R += cdict[cur_c]
+            elif cur_s[idx] == 'T':
+                T += cdict[cur_c]
+            elif cur_s[idx] == 'F':
+                F += cdict[cur_c]   
+            elif cur_s[idx] == 'C':
+                C += cdict[cur_c]   
+            elif cur_s[idx] == 'M':
+                M += cdict[cur_c]   
+            elif cur_s[idx] == 'J':
+                J += cdict[cur_c]   
+            elif cur_s[idx] == 'A':
+                A += cdict[cur_c]   
+            elif cur_s[idx] == 'N':
+                N += cdict[cur_c]   
+        
+        elif cur_c <= 3:
+            idx = 0
+            if cur_s[idx] == 'R':
+                R += cdict[cur_c]
+            elif cur_s[idx] == 'T':
+                T += cdict[cur_c]
+            elif cur_s[idx] == 'F':
+                F += cdict[cur_c]   
+            elif cur_s[idx] == 'C':
+                C += cdict[cur_c]   
+            elif cur_s[idx] == 'M':
+                M += cdict[cur_c]   
+            elif cur_s[idx] == 'J':
+                J += cdict[cur_c]   
+            elif cur_s[idx] == 'A':
+                A += cdict[cur_c]   
+            elif cur_s[idx] == 'N':
+                N += cdict[cur_c]   
+        else:
+            continue
+    answer = ''
+
+    if R >= T: answer += 'R' 
+    else: answer += 'T'
+    if C >= F: answer += 'C' 
+    else: answer += 'F'
+    if J >= M: answer += 'J' 
+    else: answer += 'M'
+    if A >= N: answer += 'A' 
+    else: answer += 'N'
     
     return answer
