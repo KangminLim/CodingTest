9db62c95aa3e8d87d0eaeb7a4f1911dc9f7aecf3
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-09-03 19:05:18 +0900

    [level 1] Title: 공원 산책, Time: 0.50 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/README.md
+++ 프로그래머스/1/172928. 공원 산책/README.md
@@ -1,10 +1,10 @@
 # [level 1] 공원 산책 - 172928 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.60 ms
+메모리: 10.4 MB, 시간: 0.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 18일 22:04:21
+2024년 09월 03일 19:05:15
 
 ### 문제 설명
 
--- 프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -1,51 +1,29 @@
-# def simul(dr,dist,park):
-#     d = -1
-#     if dr == 'N' : d = 0
-#     elif dr == 'E' : d = 1
-#     elif dr == 'S' : d = 2
-#     else: d = 3
-    
-#     dx,dy = [-1,1,0,0],[0,0,-1,1]
-#     for i in range(len(park)):
-#         for j in range(len(park[0]):
-#             if 
-    
-    
 def solution(park, routes):
-    d = -1
-    pos = (-1,-1)
-    di,dj = [-1,1,0,0],[0,0,-1,1]
     N = len(park)
     M = len(park[0])
-    new_park = [[0] * M for _ in range(N)]
-    for i in range(N):
-        for j in range(M):
-            new_park[i][j] = park[i][j]
-    for route in routes:
-        dr,dist = route.split()
-        if dr == 'N' : d = 0
-        elif dr == 'S' : d = 1
-        elif dr == 'W' : d = 2
-        else: d = 3
-        for i in range(N):
-            for j in range(M):
-                if new_park[i][j] == 'S': 
-                    pos = (i,j)
-        ci,cj = pos
-        flag = 0
-        for m in range(1,int(dist)+1):
-            ni,nj = ci + di[d]*m, cj+dj[d]*m
-            if not (0<=ni<N and 0<=nj<M):
-                flag = 1
-                break
-            elif 0<=ni<N and 0<=nj<M:
-                if new_park[ni][nj] == 'X':
-                    flag = 1
-                    break
-        if flag == 0:
-            new_park[ci][cj] = 'O'
-            new_park[ni][nj] = 'S'
-            pos = (ni,nj)
-            
-    return pos
+    for i in range(len(park)):
+        for j in range(len(park[0])):
+            if park[i][j] == 'S':
+                si,sj = i,j
+    ddict = {'N' : 0,
+            'S': 1,
+            'W': 2,
+            'E': 3}
+    di,dj = [-1,1,0,0],[0,0,-1,1]
     
+    
+    for route in routes:
+        dr, mul = route.split()
+        flag = True
+        ti,tj = si,sj
+        for m in range(1,int(mul)+1):
+            ni, nj = ti + di[ddict[dr]], tj + dj[ddict[dr]]
+            if 0<=ni<N and 0<=nj<M and park[ni][nj] != 'X':
+                ti,tj = ni,nj
+            else:
+                flag = False
+                break
+        if flag:
+            si,sj = ti,tj
+
+    return [si,sj]
