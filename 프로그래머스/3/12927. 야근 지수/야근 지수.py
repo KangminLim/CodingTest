368bba11b41cdec7f1b1a47a046e9f42cb482b00
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-09-05 18:21:07 +0900

    [level 3] Title: 야근 지수, Time: 381.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,14 +1,13 @@
+# 최대 힙 
 import heapq
 def solution(n, works):
     answer = 0
-    if n >= sum(works):
-        return 0 
-    
     works = [-w for w in works]
     heapq.heapify(works)
-    for _ in range(n):
-        i = heapq.heappop(works)
-        i += 1
-        heapq.heappush(works,i)
     
-    return sum([w**2 for w in works])
+    for _ in range(n):
+        work = heapq.heappop(works)
+        work += 1 
+        heapq.heappush(works,work)
+    
+    return sum(x**2 for x in works if x < 0)
