020681892d61881f309ca9b9f0879d2674bc0a85
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-09-10 09:52:39 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,10 +1,8 @@
 def solution(n):
-    answer = 0
-    n_one = bin(n).count('1')
-    temp = n
-    while True:
-        temp += 1
-        t_one = bin(temp).count('1')
-        if n_one == t_one:
-            return temp
- 
+    i = n
+    tmp = i
+    b_n = bin(n)
+    while i == n:
+        tmp += 1
+        if bin(n).count('1') == bin(tmp).count('1'):
+            return tmp
