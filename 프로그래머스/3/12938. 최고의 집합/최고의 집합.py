f0233fe478281af76b8056f37288ba53917eea2b
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-09-11 10:00:04 +0900

    [level 3] Title: 최고의 집합, Time: 0.81 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -0,0 +1,11 @@
+def solution(n, s):
+    answer = []
+    d,m = divmod(s,n)
+    if d == 0: return [-1]
+
+    for _ in range(n-m):
+        answer.append(d)
+    for _ in range(m):
+        answer.append(d+1)
+    
+    return answer
