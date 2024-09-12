c959aaf120bae154761d3f8572b522930f6d5a2b
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-09-12 09:55:32 +0900

    [level 1] Title: 소수 찾기, Time: 3320.61 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,13 @@
+
+def solution(n):
+    def is_prime(n):
+        for i in range(2,int(n**0.5)+1):
+            if n%i == 0:
+                return False
+        return True
+    
+    answer = 0
+    for i in range(2,n+1):
+        if is_prime(i): 
+            answer += 1
+    return answer
