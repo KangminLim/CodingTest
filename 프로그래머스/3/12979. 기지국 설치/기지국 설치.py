8c4c4d4e690f999ad5603b7abd6397273d4ac29c
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-15 01:18:57 +0900

    [level 3] Title: 기지국 설치, Time: 3.51 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -0,0 +1,20 @@
+def solution(n, stations, w):
+    answer = 0
+    need = []
+    if stations[0]-w-1 > 0:
+        need.append(stations[0]-w-1)
+    if (n-stations[-1] - w) > 0:
+        need.append(n-stations[-1]-w)
+    
+    for i in range(len(stations)-1):
+        div = stations[i+1]-stations[i] - 1
+        div -= (2*w)
+        if div > 0:
+            need.append(div)
+    
+    for i in need:
+        answer += i // (w*2+1)
+        if i%(w*2+1) != 0:
+            answer += 1
+    
+    return answer
