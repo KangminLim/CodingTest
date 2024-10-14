42d5ae123377cac36f4a2285e770119fa6eceecd
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-14 15:49:59 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,4 +1,3 @@
 def solution(s):
-    slst = list(map(int,s.split()))
-    answer =  str(min(slst)) + ' ' + str(max(slst))
-    return answer
+    s = list(map(int,s.split(' ')))
+    return str(min(s)) + ' ' + str(max(s))    
