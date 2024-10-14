3cdb38db8f9d4b4f769ad573d0dec83e1f8eadd9
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-14 23:09:01 +0900

    [level 2] Title: 숫자의 표현, Time: 6.30 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,15 +1,14 @@
 def solution(n):
-    answer = 0
-    temp = 0
+    cnt = 0
     for i in range(1,n+1):
-        temp = i
-        for j in range(i+1,n+1):
-            temp += j
-            if temp > n:
+        tmp = 0
+        for j in range(i,n+1):
+            tmp += j
+            if tmp > n:
+                tmp = 0
                 break
-            elif temp == n:
-                answer += 1
+            elif tmp == n:
+                cnt += 1
+                tmp = 0
                 break
-    # 자기자신
-    answer += 1
-    return answer
+    return cnt
