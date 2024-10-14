acc2c4778c393296917731138f612980c1c79829
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-14 18:13:44 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 9.28 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,25 +1,16 @@
 def solution(s):
-    cnt, z_cnt = 0, 0
     answer = []
+    ccnt = 0
+    zcnt = 0
     while True:
-        # 1. x의 모든 0을 제거
-        new_res = ''
+        tmp = ''
+        ccnt += 1
         for i in s:
-            if int(i) != 0:
-                new_res += i
+            if i == '1':
+                tmp += i
             else:
-                z_cnt += 1
-        new_len = len(new_res)
-    
-        t = ''
-        while new_len > 0:
-            div = divmod(new_len,2)[0]
-            mod = divmod(new_len,2)[1]
-            t += str(mod)
-            new_len = div
-        s = t[::-1]
-        cnt += 1
+                zcnt += 1
+        s = bin(len(tmp))[2:]
         if s == '1':
             break
-    
-    return [cnt,z_cnt]
+    return [ccnt,zcnt]
