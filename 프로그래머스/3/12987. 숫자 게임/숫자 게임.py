ddb5e6943aaeedc94cd329eeb4f5d22985e8c9c1
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-14 23:42:09 +0900

    [level 3] Title: 숫자 게임, Time: 39.75 ms, Memory: 18.2 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -1,11 +1,10 @@
 def solution(A, B):
-    answer = 0
     A.sort(reverse=True)
     B.sort(reverse=True)
+    answer = 0
     i = 0
     for a in A:
         if a < B[i]:
             answer += 1
             i += 1
-        
     return answer
