57119c3788bb85859389e7af7a7feebe7fe1e73c
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-15 23:46:14 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,10 +1,9 @@
 import math
 def solution(arr):
-    answer = 0 
+    answer = 0
     for i in arr:
-        # 초깃값
         if answer == 0:
             answer = i
-        else: 
-            answer = (answer*i) // math.gcd(answer,i)
+        else:
+            answer = (answer * i) // math.gcd(answer,i)
     return answer
