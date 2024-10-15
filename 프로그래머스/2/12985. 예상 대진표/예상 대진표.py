312a01a40fce873be234edd49776a0ae8e7c4dcf
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-16 01:41:07 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,9 +1,8 @@
 def solution(n,a,b):
     answer = 0
-
-    while a != b:
-        answer += 1
+    while a!=b:
         a = (a+1)//2
         b = (b+1)//2
-
+        answer += 1
+    
     return answer
