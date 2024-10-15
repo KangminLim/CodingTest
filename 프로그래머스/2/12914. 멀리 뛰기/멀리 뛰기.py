6b897e73ef3d8b389736d80661d65f1f028c361a
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-15 17:09:41 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.42 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,12 +1,9 @@
+# dp[1] = 1 / dp[2] = (1,1), (2) / dp[3] (1,1,1) (2,1) (1,2) / dp[4] = 5 
 def solution(n):
+    dp = [0] * (n+2)
+    dp[1] = 1
+    dp[2] = 2
     
-    if n == 1:
-        return 1
-    
-    a = 1 
-    b = 2
-    
-    for i in range(2,n):
-        a, b = b, a+b
-        
-    return b % 1234567
+    for i in range(3,n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]%1234567
