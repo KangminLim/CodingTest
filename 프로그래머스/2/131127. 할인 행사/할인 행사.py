7d5ed2fee15751f5f1711abf85742034a6c1f3a8
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-16 19:43:34 +0900

    [level 2] Title: 할인 행사, Time: 219.15 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -2,13 +2,13 @@ from collections import Counter
 def solution(want, number, discount):
     answer = 0
     wdict = {}
-    
     for i in range(len(want)):
         wdict[want[i]] = number[i]
-
-    for i in range(len(discount)-9):
-        c = Counter(discount[i:i+10])
-        if wdict == c:
-            answer += 1
-            
-    return answer
+    i = 0
+    while i <= len(discount) - 10:
+        tdict = Counter(discount[i:i+10])
+        if tdict == wdict:
+            answer +=1
+        i += 1
+    
+    return answer 
