e1de510b41402ea3dd432ecbe6de328067af6655
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-16 15:12:04 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4356.93 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,12 +1,9 @@
 def solution(elements):
     answer = 0
+    l = len(elements)
+    elements = 2*elements
     aset = set()
-    elen = len(elements)
-    elements = 2 * elements
-    for i in range(elen):
-        for j in range(elen):
-            # print(elements[i:j+1])
-            tmp = sum(elements[i:i+j+1])
-            aset.add(tmp)
-        
+    for j in range(1,l+1): 
+        for i in range(l):
+            aset.add(sum(elements[i:i+j]))
     return len(aset)
