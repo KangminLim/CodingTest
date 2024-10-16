2c49aca05b8b1bda2706e5d0071492f26ac1ee5e
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-16 14:46:46 +0900

    [level 2] Title: 귤 고르기, Time: 58.23 ms, Memory: 26.4 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,14 +1,10 @@
+from collections import Counter
 def solution(k, tangerine):
     answer = 0
-    t_dict = {}
-    for tan in tangerine:
-        if tan not in t_dict:
-            t_dict[tan] = 1
-        else:
-            t_dict[tan] += 1
-    tlst = sorted(t_dict.items(),key = lambda x:x[1])
-    while k>0:
-        now = tlst.pop()[1]
-        k -= now
+    tdict = Counter(tangerine)
+    tlst = sorted(tdict.items(), key = lambda x:x[1])
+    
+    while k > 0:
+        k -= tlst.pop()[1]
         answer += 1
     return answer
