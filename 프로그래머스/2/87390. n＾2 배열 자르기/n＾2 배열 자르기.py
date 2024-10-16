c0f3a877a5ca8fdd706984732a75bb5883a0d576
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-16 20:12:16 +0900

    [level 2] Title: n^2 배열 자르기, Time: 55.80 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -1,7 +1,10 @@
+    # 123   1234
+    # 223   2234
+    # 333   3334
+    #       4444
 def solution(n, left, right):
     answer = []
     for i in range(left,right+1):
         div,mod = divmod(i,n)
         answer.append(max(div,mod)+1)
-
     return answer
