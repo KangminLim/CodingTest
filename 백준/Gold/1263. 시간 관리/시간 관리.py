4e4495763c26f5c2a3bb9f8eb9410634805496e0
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-17 19:52:06 +0900

    [Gold V] Title: 시간 관리, Time: 56 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/1263. 시간 관리/시간 관리.py
+++ 백준/Gold/1263. 시간 관리/시간 관리.py
@@ -1,17 +1,16 @@
-# 시간 관리
 N = int(input())
 lst = [list(map(int,input().split())) for _ in range(N)]
-lst.sort(key=lambda x:x[1], reverse=True)
+lst.sort(key=lambda x:(x[1]),reverse=True)
 
 ans = lst[0][1] - lst[0][0]
 
-for ti,si in lst[1:]:
+for ti, si in lst[1:]:
     if ans > si:
-        ans = si-ti
+        ans = si - ti
     else:
         ans -= ti
 
-if ans >=0:
+if ans >= 0:
     print(ans)
 else:
     print(-1)
