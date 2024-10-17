1d8b0358a7ab068720fe0d6f097f806ffbc428c8
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-18 00:47:31 +0900

    [Gold V] Title: 풍선 맞추기, Time: 264 ms, Memory: 216428 KB -BaekjoonHub

--- 백준/Gold/11509. 풍선 맞추기/풍선 맞추기.py
+++ 백준/Gold/11509. 풍선 맞추기/풍선 맞추기.py
@@ -2,11 +2,11 @@ N = int(input())
 lst = list(map(int,input().split()))
 positions = [0] * (1000001)
 
-for i in range(N):
+for i in range(len(lst)):
+    # print(lst[i],lst[i-1])
     if positions[lst[i]] == 0:
         positions[lst[i]-1] += 1
     else:
         positions[lst[i]-1] += 1
         positions[lst[i]] -= 1
-
 print(sum(positions))
