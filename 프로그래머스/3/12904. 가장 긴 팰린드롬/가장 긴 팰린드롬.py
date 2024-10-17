ee940744484c0b93d8451b4e1c23527cd1dc698b
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-17 15:30:39 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 3595.47 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -0,0 +1,8 @@
+def solution(s):
+    answer = 1
+
+    for i in range(len(s)):
+        for j in range(i+1,len(s)+1):
+            if s[i:j] == s[i:j][::-1]:
+                answer = max(answer,len(s[i:j]))
+    return answer
