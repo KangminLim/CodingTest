86d76e01fc0a78fad937a745376c29d0be7c23c2
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-17 18:24:17 +0900

    [Gold V] Title: 감소하는 수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.py
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.py
@@ -1,15 +1,14 @@
 from itertools import combinations
 N = int(input())
-
-ans = []
+answer = []
 for i in range(1,11):
-    for combination in combinations(range(10),i):
-        num = int(''.join([str(s) for s in sorted(combination,reverse=True)]))
-        ans.append(num)
+    for comb in combinations(range(10),i):
+        tmp = int(''.join([str(s) for s in sorted(comb,reverse=True)]))
+        answer.append(tmp)
+# print(answer)
+answer.sort()
 
-ans.sort()
-
-if N < len(ans):
-    print(ans[N])
+if N < len(answer):
+    print(answer[N])
 else:
     print(-1)
