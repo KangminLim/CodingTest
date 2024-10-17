1bbcec37520fd2d020b5bc7bf672e83edf3fe9a9
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-18 01:58:30 +0900

    [Gold V] Title: 벼락치기, Time: 344 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.py
+++ 백준/Gold/14728. 벼락치기/벼락치기.py
@@ -6,6 +6,6 @@ dp[0] = 0
 lst = [list(map(int,input().split())) for _ in range(N)]
 
 for k, s in lst:
-    for i in range(T,k-1,-1): # 메모리, 절약을 위해 Top-bottom으로
-        dp[i] = max(dp[i],dp[i-k]+s)
+    for i in range(T,k-1,-1): # 메모리 절약을 위해 Top-bottom으로
+        dp[i] = max(dp[i],dp[i-k] + s)
 print(dp[-1])
