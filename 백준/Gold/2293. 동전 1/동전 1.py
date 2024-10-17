43d69a6e4e2d7ef5ce7ea066db25492e1cb676ba
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-17 20:44:20 +0900

    [Gold IV] Title: 동전 1, Time: 164 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,11 +1,11 @@
-n, k = map(int,input().split())
-coins = [int(input()) for _ in range(n)]
+N, K = map(int,input().split())
+coins = []
+for _ in range(N):
+    coins.append(int(input()))
 
-dp = [0] * (k+1)
+dp = [0] * (K+1)
 dp[0] = 1
-
 for coin in coins:
-    for i in range(1,k+1):
-        if (i - coin) >= 0:
-            dp[i] += dp[i-coin]
-print(dp[k])
+    for i in range(coin,K+1):
+        dp[i] += dp[i-coin]
+print(dp[-1])
