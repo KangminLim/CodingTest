fed85c070cb74329203a0c522b2702fa21abace5
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-17 15:19:39 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,13 +1,15 @@
 def solution(clothes):
-    clo = {}
-    for clothe in clothes:
-        if clothe[1] not in clo:
-            clo[clothe[1]] = 1
-        else:
-            clo[clothe[1]] += 1
-            
     answer = 1
-    for idx, val in clo.items():
-        answer *= (val+1) 
-    
-    return answer-1
+    cdict = {}
+    for clothe in clothes:
+        name, genre = clothe
+        if genre not in cdict:
+            cdict[genre] = [name]
+        else:
+            cdict[genre].append(name)
+    tlst = []
+    for key in cdict:
+        answer *= len(cdict[key])+1
+    answer -=1 
+            
+    return answer
