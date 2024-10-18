29ec3bbfa4c2c71e77c6e25dd3e196d354f4312d
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-18 15:18:28 +0900

    [Gold V] Title: 동전 2, Time: 288 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,11 +1,9 @@
-# 동전 2
 N, K = map(int,input().split())
-coins = set()
-for i in range(N):
-    coin = int(input())
-    if coin <= K:
-        coins.add(coin)
 
+coins = set()
+
+for i in range(N):
+    coins.add(int(input()))
 
 dp = [100001] * (K+1)
 
@@ -13,6 +11,6 @@ dp[0] = 0
 
 for coin in coins:
     for i in range(coin,K+1):
-        dp[i] = min(dp[i], dp[i-coin]+1)
-
-print(dp[-1] if dp[-1] != 100001 else -1)
+        dp[i] = min(dp[i],dp[i-coin]+1)
+if dp[-1] != 100001: print(dp[-1])
+else: print(-1)
