d38d9e9c1ad147d6d5da9d4e85d901f5d225d00d
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-18 16:16:52 +0900

    [Gold V] Title: 내려가기, Time: 292 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -1,24 +1,18 @@
 import sys
-
 input = sys.stdin.readline
-
-# 배열 크기 입력 받기
 N = int(input())
 
 a,b,c = map(int,input().split())
 
 dp = [[] for _ in range(2)]
 
-dp[0] = [a,b,c]
-dp[1] = [a,b,c]
+dp[0] = [a,b,c] # 최소
+dp[1] = [a,b,c] # 최대
 
 for _ in range(N-1):
     a,b,c = map(int,input().split())
-
-    # 최대
-    dp[0][0], dp[0][1], dp[0][2] = a+max(dp[0][0],dp[0][1]),b+max(dp[0][0], dp[0][1], dp[0][2]),c+max(dp[0][1], dp[0][2])
-    # 최소
-    dp[1][0], dp[1][1], dp[1][2] = a+min(dp[1][0],dp[1][1]),b+min(dp[1][0], dp[1][1], dp[1][2]),c+min(dp[1][1], dp[1][2])
-
-
-print(max(dp[0]),min(dp[1]),end=' ')
+    # dp[0][0],dp[0][1],dp[0][2] = min(dp[0][0]+a,dp[0][0]+b), min(dp[0][0]+a,dp[0][1]+b,dp[0][2]+c), min(dp[0][2]+b,dp[0][2]+c)
+    # dp[1][0],dp[1][1],dp[1][2] = max(dp[1][0]+a,dp[1][0]+b), max(dp[1][0]+a,dp[1][1]+b,dp[1][1]+c), max(dp[1][2]+b,dp[1][2]+c)
+    dp[0][0], dp[0][1], dp[0][2] = a+min(dp[0][0],dp[0][1]), b+min(dp[0][0],dp[0][1],dp[0][2]), c+min(dp[0][1],dp[0][2])
+    dp[1][0], dp[1][1], dp[1][2] = a+max(dp[1][0],dp[1][1]), b+max(dp[1][0],dp[1][1],dp[1][2]), c+max(dp[1][1],dp[1][2])
+print(max(dp[1]),min(dp[0]))
