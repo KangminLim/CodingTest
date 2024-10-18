d814fb91634a0075657d82164889b28191e0d99a
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-18 19:03:26 +0900

    [Gold V] Title: 개똥벌레, Time: 392 ms, Memory: 70676 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.py
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.py
@@ -1,32 +1,29 @@
 import sys
 input = sys.stdin.readline
-
 N, H = map(int,input().split())
-down = [0] * (H+1)
+
 up = [0] * (H+1)
+down = [0] * (H+1)
 
-# 장애물 입력
-for i in range(N):
-    h = int(input())
-
+for i in range(1,N+1):
     if i % 2 == 0:
-        down[h] += 1
+        down[int(input())] += 1
     else:
-        up[h] += 1
-# print(down,up)
+        up[int(input())] += 1
+
 # 인덱스 역순으로 누적합 계산
 for i in range(H-1,0,-1):
     down[i] += down[i+1]
     up[i] += up[i+1]
+
 ans_mn = N
 ans_h = 0
-# print(down,up)
+
 for i in range(1,H+1):
-    # print('')
-    # print(i, H-i+1)
     if ans_mn > down[i] + up[H-i+1]:
         ans_mn = down[i] + up[H-i+1]
         ans_h = 1
     elif ans_mn == down[i] + up[H-i+1]:
         ans_h += 1
+
 print(ans_mn,ans_h)
