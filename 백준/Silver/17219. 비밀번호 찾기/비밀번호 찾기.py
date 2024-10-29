73b9dd6d2c0d0c6149d1c08caf3e2cfb0e614dc0
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-29 23:39:44 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 7856 ms, Memory: 49208 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -0,0 +1,13 @@
+import sys
+# input = sys.stdin.readline
+N, M = map(int,input().split())
+tdict = {}
+
+for _ in range(N):
+    k,v = map(str,input().split())
+    tdict[k] = v
+
+for _ in range(M):
+    k = str(input())
+    print(tdict[k])
+
