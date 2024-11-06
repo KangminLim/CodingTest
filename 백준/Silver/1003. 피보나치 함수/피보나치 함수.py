4a3bf9dd1ea898dccb8bda908061934caf27096f
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-11-06 15:54:57 +0900

    [Silver III] Title: 피보나치 함수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,13 +1,12 @@
-T = int(input())
-for _ in range(T):
-    tc = int(input())
-    if tc == 0:
-        print(1, 0)
-        continue
-    elif tc == 1:
-        print(0, 1)
-        continue
-    z_cnt, o_cnt = 1, 1
-    for _ in range(2,tc):
-        z_cnt, o_cnt = o_cnt, o_cnt+z_cnt
-    print(z_cnt, o_cnt)
+N = int(input())
+for _ in range(N):
+    num = int(input())
+    if num == 0:
+        print(1,0)
+    elif num == 1:
+        print(0,1)
+    else:
+        zcnt = ocnt = 1
+        for i in range(2,num):
+            zcnt,ocnt = ocnt, zcnt+ocnt
+        print(zcnt,ocnt)
