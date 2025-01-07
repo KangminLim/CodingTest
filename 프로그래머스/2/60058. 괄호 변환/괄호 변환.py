98c9a95570d6ed36e042f80bff3aea50c19cf392
KangMin <87487729+KangminLim@users.noreply.github.com>
2025-01-07 22:19:42 +0900

    [level 2] Title: 괄호 변환, Time: 0.67 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
@@ -1,6 +1,4 @@
 '''
-'(' 와 ')' 로만 이루어진 문자열이 있을 경우, '(' 의 개수와 ')' 의 개수가 같다면 이를 균형잡힌 괄호 문자열이라고 부릅니다.
-그리고 여기에 '('와 ')'의 괄호의 짝도 모두 맞을 경우에는 이를 올바른 괄호 문자열이라고 부릅니다.
 1. 입력이 빈 문자열인 경우, 빈 문자열을 반환합니다. 
 2. 문자열 w를 두 "균형잡힌 괄호 문자열" u, v로 분리합니다. 단, u는 "균형잡힌 괄호 문자열"로 더 이상 분리할 수 없어야 하며, v는 빈 문자열이 될 수 있습니다. 
 3. 문자열 u가 "올바른 괄호 문자열" 이라면 문자열 v에 대해 1단계부터 다시 수행합니다. 
@@ -12,37 +10,40 @@
   4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다. 
   4-5. 생성된 문자열을 반환합니다.
 '''
-# 균형잡힌 괄호 문자열 인덱스 반환
-def check_balance(p):
+def check_proper(s):
+    stack = []
+    for ch in s:
+        if ch == '(':
+            stack.append(ch)
+        elif stack and ch == ')':
+            stack.pop()
+    if stack:
+        return False
+    else:
+        return True
+
+def check_balance(s):
     cnt = 0
-    for i in range(len(p)):
-        if p[i] == '(':
+    for i in range(len(s)):
+        if s[i] == '(':
             cnt += 1
         else:
-            cnt -= 1 
+            cnt -= 1
         if cnt == 0:
             return i
-
-# 올바른 문자열
-def check_proper(p):
-    cnt = 0
-    for i in p:
-        if i == '(':
-            cnt += 1
-        else:
-            if cnt == 0:
-                return False # 쌍이 맞지 않는 경우
-            cnt -= 1
-    return True
     
-
+    
 def solution(p):
     answer = ''
+    # 1
     if p == '': return ''
-    idx = check_balance(p)
-    u = p[:idx+1]
-    v = p[idx+1:]
+
+    # 2
+    i = check_balance(p)
+    u = p[:i+1]
+    v = p[i+1:]
     
+    # 3
     if check_proper(u):
         answer = u + solution(v)
     else:
@@ -56,6 +57,4 @@ def solution(p):
             else:
                 u[i] = '('
         answer += ''.join(u)
-                    
-
     return answer
