32d26c137f604fce997d519296ece168441e1ee4
KangMin <87487729+KangminLim@users.noreply.github.com>
2025-01-23 22:55:21 +0900

    [Gold V] Title: 멀티버스 Ⅱ, Time: 680 ms, Memory: 77704 KB -BaekjoonHub

--- 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.py
+++ 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.py
@@ -1,24 +1,23 @@
-import sys, math
 from collections import defaultdict
-input = sys.stdin.readline
-
+import math
 M, N = map(int,input().split())
-lst = [list(map(int,input().split())) for _ in range(M)]
 
+lst = [list(map(int,input().split())) for _ in range(M)]
 idict = defaultdict(int)
 
 for items in lst:
-    items = {item : i for i, item in enumerate(items)}
+    items = {item : i for i,item in enumerate(items)}
     # print(items)
-    index = [items[item] for item in sorted(items.keys())]
+    index = [items[i] for i in sorted(items.keys())]
     # print(index)
     key = ''.join(list(map(str,index)))
     # print(key)
     idict[key] += 1
-    # print(idict)
 
-answer = 0
-for index in idict:
-    if idict[index] > 1:
-        answer += math.comb(idict[index],2)
-print(answer)
+ans = 0
+
+for key in idict:
+    if idict[key] > 1:
+        ans += math.comb(idict[key],2)
+
+print(ans)
