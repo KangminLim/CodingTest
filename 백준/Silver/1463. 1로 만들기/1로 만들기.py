041d0e9dd4afdd97d402c5f797b3d3b418c9b630
KangMin <87487729+KangminLim@users.noreply.github.com>
2025-01-28 22:33:24 +0900

    [Silver III] Title: 1로 만들기, Time: 468 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,15 +1,14 @@
-# 정수를 1로 만들기
 import sys
 input = sys.stdin.readline
 N = int(input())
-D = [0] * (N+1)
-D[1] = 0
+dp = [0] * (N+1)
 
 for i in range(2,N+1):
-    D[i] = D[i-1] + 1
-    if i % 2 == 0:
-        D[i] = min(D[i], D[i//2]+1)
-    if i % 3 == 0:
-        D[i] = min(D[i], D[i//3]+1)
+    dp[i] = dp[i-1] + 1
 
-print(D[N])
+    if i%3 == 0:
+        dp[i] = min(dp[i], dp[i//3]+1)
+    if i%2 == 0:
+        dp[i] = min(dp[i],dp[i//2]+1)
+# print(dp)
+print(dp[N])
