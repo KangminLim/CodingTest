307ff2ad6d79d111cfa27e4349bc9f28b7cd40fc
KangMin <87487729+KangminLim@users.noreply.github.com>
2025-05-13 11:14:55 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 6.71 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
@@ -5,30 +5,31 @@ using namespace std;
 
 vector<int> solution(vector<int> sequence, int k) {
     vector<int> answer(2,0);
-    int s = 0, e = 0, tmp = sequence[0];
+    int s=0,e=0;
     int mn = 10000001;
     int slen = sequence.size();
-    while (s<=e && e < slen) {
+    int tmp = sequence[0];
+    
+    while (s<=e && e<slen){
         if (tmp == k) {
-            if (e-s+1< mn) {
+            if (e-s+1 < mn) {
                 mn = e-s+1;
                 answer[0] = s;
                 answer[1] = e;
-                }
+            }
             tmp -= sequence[s];
             s++;
-            }
+        }    
         else if (tmp > k) {
             tmp -= sequence[s];
             s++;
         }
         else if (tmp < k) {
             e++;
-            if (e < slen) {
+            if (e < slen) { 
                 tmp += sequence[e];
             }
         }
     }
-    
     return answer;
 }
